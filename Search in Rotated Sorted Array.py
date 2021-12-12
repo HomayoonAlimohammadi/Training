8a102a671f34090a681127f29350276585c03390
Homayoon Alimohammadi <homayoonalimohammadi@gmail.com>
2021-12-12 11:06:12 +0330

    Dec 12th 2021

---
+++ Search in Rotated Sorted Array.py
@@ -0,0 +1,12 @@
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        try:
+            return nums.index(target)
+        except:
+            return -1
+        
+S = Solution()
+nums = [1,2,3,4]
+target = 0
+ans = S.search(nums,target)
+print(ans)
