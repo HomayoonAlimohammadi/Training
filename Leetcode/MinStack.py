cebc7a914497e53297a36d62cf4c24beaf0ae70f
HomayoonAlimohammadi <homayoonalimohammadi@gmail.com>
2022-05-16 16:23:55 +0430

    Add MinStack.py

---
+++ Leetcode/MinStack.py
@@ -0,0 +1,21 @@
+class MinStack:
+
+    def __init__(self):
+        self.min_stack = []
+        self.stack = []
+
+    def push(self, val: int) -> None:
+        self.stack.append(val)
+        if not self.min_stack or val <= self.min_stack[-1]:
+            self.min_stack.append(val)
+
+    def pop(self) -> None:
+        val = self.stack.pop()
+        if self.min_stack and val == self.min_stack[-1]:
+            self.min_stack.pop()
+
+    def top(self) -> int:
+        return self.stack[-1]
+
+    def getMin(self) -> int:
+        return self.min_stack[0]        
