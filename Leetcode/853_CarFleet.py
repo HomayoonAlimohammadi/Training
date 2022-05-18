a242e9653d0e271b63d49f59daab954ee9c976c8
HomayoonAlimohammadi <homayoonalimohammadi@gmail.com>
2022-05-18 22:53:55 +0430

    Update CarFleet

--- Leetcode/853_CarFleet.py
+++ Leetcode/853_CarFleet.py
@@ -1,4 +1,5 @@
 from typing import List
+import unittest
 
 
 class Solution:
@@ -21,3 +22,35 @@ class Solution:
             stack.append(record)
         
         return len(stack)
+
+
+class TestCases(unittest.TestCase):
+
+    def setUp(self):
+        self.sol = Solution().carFleet
+
+    def test_solution_1(self):
+        target = 12
+        position = [10,8,0,5,3]
+        speed = [2,4,1,1,3]
+        ans = self.sol(target, position, speed)
+        expected = 3
+        self.assertEqual(ans, expected)
+
+        target = 100
+        position = [0,2,4]
+        speed = [4,2,1]
+        ans = self.sol(target, position, speed)
+        expected = 1
+        self.assertEqual(ans, expected)
+
+        target = 10
+        position = [3]
+        speed = [3]
+        ans = self.sol(target, position, speed)
+        expected = 1
+        self.assertEqual(ans, expected)
+
+
+if __name__ == '__main__':
+    unittest.main()
