bffba4892e1abe4bff000e5a4122f17a2e96c4b9
HomayoonAlimohammadi <homayoonalimohammadi@gmail.com>
2022-05-23 18:46:33 +0430

    Update BalancedTree

--- Leetcode/110_BalancedTree.py
+++ Leetcode/110_BalancedTree.py
@@ -1,4 +1,5 @@
 from typing import Optional
+from random import sample
 
 
 class TreeNode:
@@ -11,22 +12,51 @@ class TreeNode:
     def __str__(self):
         return f'TreeNode{{val: {self.val}, left: {self.left}, right: {self.right}}}'
 
+    def __repr__(self):
+        return f'{self.val}'
+
 
 class Solution:
+
+    def isBalanced_search(self, root: Optional[TreeNode]) -> bool:
+        '''Determine wheter a Tree structure is balanced or not.'''
+
+        nodes = [(1, root)]
+        leafs = []
+        all_depth = []
+        while nodes:
+            node = nodes[0][1]
+            depth = nodes[0][0]
+            has_left, has_right = False, False
+            if node.left:
+                nodes.append((depth+1, node.left))
+                has_left = True
+            if node.right:
+                nodes.append((depth+1, node.right))
+                has_right = True
+            print(node.val, has_left, has_right)
+            if not has_left or not has_right:
+                if hasattr(node, 'val'):
+                    leafs.append(node.val)
+                    all_depth.append(depth)
+                else:
+                    leafs.append(None)
+                
+                if all_depth and all_depth[-1] - all_depth[0] > 1:
+                    print(all_depth)
+                    print(leafs)
+                    return False
+            nodes.pop(0)
+
+        print(all_depth)
+        return True 
 
     def isBalanced(self, root: Optional[TreeNode]) -> bool:
         '''Determine wheter a Tree structure is balanced or not.'''
 
-        leafs = [root]
-        while leafs:
-            node = leafs[0]
-            if node.left:
-                leafs.append(node.left)
-            if node.right:
-                leafs.append(node.right)
-            leafs.pop(0)
-
-        return leafs
+        nodes = [root]
+        while nodes:
+            pass
 
 
 def create_tree(values):
@@ -52,9 +82,43 @@ def create_tree(values):
                 curr = curr.right
     return root
 
+def create_arbitrary_tree(values):
 
-values = [1,2,3,4]
-root = create_tree(values)
+    root = TreeNode(values[0])
+    nodes = [root]
+    values.pop(0)
+    while nodes:
+        
+        node = nodes[0]
+        nodes.pop(0)
+
+        if not values:
+            break
+
+        left_val = values[0]
+        values.pop(0)
+        if left_val:
+            left_node = TreeNode(left_val)
+            nodes.append(left_node)
+            node.left = left_node
+
+        if not values:
+            break
+
+        right_val = values[0]
+        values.pop(0)
+        if right_val:
+            right_node = TreeNode(right_val)
+            nodes.append(right_node)
+            node.right = right_node
+
+    return root
+
+
+values = [3,9,20,None,None,15,7]
+# values = sample(range(1, 200), 100)
+root = create_arbitrary_tree(values)
 print(root)
+# print(Solution().isBalanced_search(root))
                 
                 
