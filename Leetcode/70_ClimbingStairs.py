a4c7c0a8ccad84092248c4c541566902432f8d80
HomayoonAlimohammadi <homayoonalimohammadi@gmail.com>
2022-05-26 17:31:13 +0430

    Update ClimbingStairs

--- Leetcode/70_ClimbingStairs.py
+++ Leetcode/70_ClimbingStairs.py
@@ -5,21 +5,48 @@ class Solution:
 
     def climbStairs(self, n: int) -> int:
 
+        def step(n_left, cache=None):
+            
+            if cache is None:
+                cache = {}
+
+            if n_left <= 0:
+                if n_left == 0:
+                    cache[0] = 1
+                    return 1
+                return 0
+
+            if n_left in cache:
+                return cache[n_left]
+            
+            ans = step(n_left - 1, cache) + step(n_left - 2, cache)
+            cache[n_left] = ans
+            return ans
+
+        return step(n)
+    
+    def climbStairs_lru(self, n: int) -> int:
+
         @lru_cache
-        def step(n_left, ways=0):
+        def step(n_left):
             
             if n_left <= 0:
                 if n_left == 0:
-                    ways += 1
-                return ways
+                    return 1
+                return 0
             
             return step(n_left - 1) + step(n_left - 2)
 
-        ways = 0
-        ways = step(n, ways)
-        
-        return ways
+        return step(n)
+
+    def climbStairsDynamic(self, n: int) -> int:
+
+        n1, n2 = 1, 1
+        for i in range(n-1):
+            n2, n1 = n1+n2, n2
+
+        return n2
 
 
-climb = Solution().climbStairs
-print(climb(45))
+climb = Solution().climbStairsDynamic
+print(climb(20))
