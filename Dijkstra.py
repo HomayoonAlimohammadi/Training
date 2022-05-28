9b1225871bd9ab672a2e63b58f15c6e6cf235014
HomayoonAlimohammadi <homayoonalimohammadi@gmail.com>
2022-05-28 10:35:54 +0430

    Update Dijkstra

--- Dijkstra.py
+++ Dijkstra.py
@@ -1,5 +1,5 @@
 from __future__ import annotations
-from typing import List, Set
+from typing import List, Set, Union
 import heapq
 
 
@@ -13,8 +13,10 @@ class Node:
         self.edges: Set[Edge] = set()
 
     def get_edge_to(self, dest: Node) -> Edge:
+        if not isinstance(dest, Node):
+            raise ValueError('`dest` must be an instance of Node.')
         for edge in self.edges:
-            if edge.dest == dest:
+            if dest in edge.nodes:
                 return edge
         raise ValueError(f'No edge from `{self.val}` to `{dest}` found.')
 
@@ -30,15 +32,16 @@ class Edge:
     Edge class for making connections between instances of Nodes.
     '''
     def __init__(self, org_node: Node, dest_node: Node, weight: int = 1) -> None:
-        self.org = org_node
-        self.dest = dest_node
+        self.nodes: Set[Node] = {org_node, dest_node}
         self.weight = weight
 
     def __str__(self) -> None:
-        return f'Edge{{from: {self.org.val}, to: {self.dest.val}, weight: {self.weight}}}'
+        org, dest = self.nodes
+        return f'Edge{{between {org.val} and {dest.val}, weight: {self.weight}}}'
     
     def __repr__(self) -> None:
-        return f'({self.org.val}, {self.dest.val})'
+        org, dest = self.nodes
+        return f'({org.val}, {dest.val})'
 
 
 class Graph:
@@ -63,10 +66,13 @@ class Graph:
         self.nodes.add(node)
         return self
 
-    def add_edge(self, org: int, dest: int, weight: int = 1) -> Graph:
+    def add_edge(self, org_node: Union[Node, int], dest_node: Union[int, Node], weight: int = 1) -> Graph:
         '''Add an edge between two given nodes, given their values.'''
-        org_node = self.get_node_by_value(org)
-        dest_node = self.get_node_by_value(dest)
+
+        if isinstance(org_node, int):
+            org_node = self.get_node_by_value(org_node)
+        if isinstance(dest_node, int):
+            dest_node = self.get_node_by_value(dest_node)
 
         edge = Edge(org_node, dest_node, weight)
 
@@ -87,23 +93,28 @@ class Graph:
     def dijkstra(self, source: int) -> List[int]:
         '''Calculate and return shortest path from a given node to all other nodes.'''
         visited = set()
-        distances = [(0, source)]
+        distances = {source: 0}
+        pq = [(0, source)]
         for node in self.nodes:
             val = node.val
-            if val in distances:
+            if val == source:
                 continue
-            distances.append((float('inf'), val))
-        heapq.heapify(distances)
+            distances[val] = float('inf')
+        heapq.heapify(pq)
         
-        while distances:
-            current_dist, val = heapq.heappop(distances)
+        while pq:
+            current_dist, val = heapq.heappop(pq)
+            if val in visited:
+                continue
+            visited.add(val)
             node = self.get_node_by_value(val)
             for adj_node in node.adj:
-                result = [d for d in distances if d[1] == adj_node.val][0]
-                adj_dist, _ = result
+                adj_dist = distances[adj_node.val]
                 edge = node.get_edge_to(adj_node)
-                if current_dist > adj_dist + edge.weight:
-                    ...
+                if current_dist + edge.weight < adj_dist:
+                    distances[adj_node.val] = current_dist + edge.weight
+                heapq.heappush(pq, (distances[adj_node.val], adj_node.val))
+        return distances
 
     def __str__(self) -> None:
         result = 'Graph: {\n'
@@ -116,6 +127,34 @@ class Graph:
     
 
 graph = Graph()
+graph.add_node(0)
 graph.add_node(1)
 graph.add_node(2)
-graph.add_edge(1, 2)
+graph.add_node(3)
+graph.add_node(4)
+graph.add_node(5)
+graph.add_node(6)
+graph.add_node(7)
+graph.add_node(8)
+graph.add_edge(0, 1, 4)
+graph.add_edge(0, 6, 7)
+graph.add_edge(1, 2, 9)
+graph.add_edge(1, 7, 20)
+graph.add_edge(1, 6, 11)
+graph.add_edge(2, 4, 2)
+graph.add_edge(2, 3, 6)
+graph.add_edge(3, 4, 10)
+graph.add_edge(3, 5, 5)
+graph.add_edge(4, 5, 15)
+graph.add_edge(4, 7, 1)
+graph.add_edge(4, 8, 5)
+graph.add_edge(5, 8, 12)
+graph.add_edge(6, 7, 1)
+graph.add_edge(7, 8, 3)
+node_1 = graph.get_node_by_value(1)
+node_2 = graph.get_node_by_value(2)
+# print(node_2.get_edge_to(node_1))
+distances = graph.dijkstra(0)
+for key in sorted(distances.keys()):
+    print(f'{key}: {distances[key]}')
+# print(graph)
