f8b3c6a8b70480c5efd288d73ab0bb63c1e0f665
HomayoonAlimohammadi <homayoonalimohammadi@gmail.com>
2022-06-06 11:57:28 +0430

    Add MergeIntervals

---
+++ Leetcode/56_MergeIntervals.py
@@ -0,0 +1,25 @@
+from typing import List
+
+
+class Solution:
+
+    def merge(self, intervals: List[List[int]]) -> List[List[int]]:
+
+        intervals.sort(key = lambda x: x[0])
+
+        n_s = []
+        i = 0
+        while i < len(intervals):
+            inter = intervals[i]
+            while i < len(intervals) - 1 and inter[1] >= intervals[i+1][0]:
+                i += 1
+                
+            n_s.append(inter)
+            i += 1
+
+        return n_s
+
+
+func = Solution().merge
+intervals = [[1,3],[2,6],[8,10],[15,18]]
+print(func(intervals))
