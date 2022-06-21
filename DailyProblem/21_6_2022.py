290f2a16b9a9a29a20b902eeb0adcfd26e18a9cb
Homayoon Alimohammadi <homayoonalimohammadi@gmail.com>
2022-06-21 11:31:07 +0430

    Update daily problem 21 june

--- DailyProblem/21_6_2022.py
+++ DailyProblem/21_6_2022.py
@@ -15,5 +15,32 @@ def find_smallest_integer(nums: List[int]) -> int:
         start = nums[i]
 
 
+def find_smallest_integer(nums: List[int]) -> int:
+    """Return smallest positive integer not available in a sequence"""
+    nums[0] = "*"
+    for i in range(1, len(nums)):
+        if 0 <= nums[i] < len(nums):
+            nums[nums[i]] = "*"
+
+    for i in range(len(nums)):
+        if nums[i] != "*":
+            return i
+
+    return len(nums)
+
+
 nums = [3, 4, -1, 1]
 print(find_smallest_integer(nums))
+print(find_smallest_integer(nums) == 0)
+
+nums = [3, 4, -1, 1]
+print(find_smallest_integer(nums))
+print(find_smallest_integer(nums) == 0)
+
+nums = [3, 4, -1, 1]
+print(find_smallest_integer(nums))
+print(find_smallest_integer(nums) == 0)
+
+nums = [3, 4, -1, 1]
+print(find_smallest_integer(nums))
+print(find_smallest_integer(nums) == 0)
