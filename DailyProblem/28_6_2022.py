4a31bcf6a063e2c9131f3af5f364acb57cd89b95
Homayoon Alimohammadi <homayoonalimohammadi@gmail.com>
2022-06-29 10:29:20 +0430

    Update daily problem 28 june, trie search results with hits

--- DailyProblem/28_6_2022.py
+++ DailyProblem/28_6_2022.py
@@ -1,14 +1,19 @@
 from __future__ import annotations
-from typing import Dict, List
+from typing import Dict, List, Set
 
 
 class Node:
     def __init__(
-        self, val: str, children: Dict[str, Node] = None, is_end: bool = False
+        self,
+        val: str,
+        children: Dict[str, Node] = None,
+        is_end: bool = False,
+        n_hits: int = 0,
     ) -> None:
         self.val = val
         self.children = {} if children is None else children
         self.is_end = is_end
+        self.n_hits = n_hits
 
     def __str__(self) -> str:
         return f"""Node(val: {self.val},
@@ -34,6 +39,7 @@ class Trie:
             current = current.children[char]
 
         current.is_end = True
+        current.n_hits += 1
 
     def __str__(self) -> str:
         return str(self.root)
@@ -46,23 +52,22 @@ class Trie:
             except KeyError:
                 return []
 
-        print(f"{current=}")
-
-        def dfs(node: Node, until_now: str, result: List[str]) -> List[str]:
+        def dfs(node: Node, until_now: str, result: Set[str]) -> List[str]:
 
             if node.is_end:
-                result.append(until_now)
+                result.add((until_now, node.n_hits))
 
             for child in node.children.values():
-                result += dfs(child, until_now + child.val, result.copy())
+                result.union(dfs(child, until_now + child.val, result))
 
             return result
 
-        return dfs(current, prefix, [])
+        results = dfs(current, prefix, set())
+        return sorted(list(results), key=lambda x: (-x[1], x[0]))
 
 
 trie = Trie()
-words = ["dog", "deer", "deal"]
+words = ["dog", "deer", "deal", "dig", "dog"]
 for word in words:
     trie.add_string(word)
 
