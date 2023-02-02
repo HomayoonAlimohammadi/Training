81bd22db98c8ef79aa0bf1b729cb200f67e6dba3
Homayoon Alimohammadi <homayoon.alimohammadi@divar.ir>
2023-02-02 13:19:10 +0330

    Add Replace Elements with Greatest Element on Right Side

--- golang/arrays_hashing/main.go
+++ golang/arrays_hashing/main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"training/arrays_hashing/anagram"
 	"training/arrays_hashing/duplicate"
+	"training/arrays_hashing/replace"
 )
 
 func main() {
@@ -15,7 +16,8 @@ Loop:
 		fmt.Println("Input the number of problem you are looking for:\n" +
 			"0. Exit \n" +
 			"1. Contains Duplicate \n" +
-			"2. Valid Anagram")
+			"2. Valid Anagram \n" +
+			"3. Replace Elements with Greatest Element on Right Side")
 		fmt.Scanln(&q)
 
 		switch q {
@@ -27,6 +29,9 @@ Loop:
 			break Loop
 		case "2":
 			anagram.IsValid()
+			break Loop
+		case "3":
+			replace.Elements()
 			break Loop
 		default:
 			fmt.Println("Invalid problem number, try again...")
---
+++ golang/arrays_hashing/replace/replace.go
@@ -0,0 +1,47 @@
+package replace
+
+import (
+	"fmt"
+	"time"
+)
+
+// Given an array arr, replace every element in that array with the greatest
+// element among the elements to its right, and replace the last element with -1.
+//
+// After doing so, return the array.
+//
+// LeetCode: https://leetcode.com/problems/replace-elements-with-greatest-element-on-right-side/
+func Elements() {
+	t0 := time.Now()
+	fmt.Println()
+	fmt.Println("Running Replace Elements...")
+	fmt.Println()
+
+	arr := []int{17, 18, 5, 4, 6, 1}
+	fmt.Println("Input:", arr)
+	ans := elements(arr)
+	fmt.Println("Answer 1:", ans)
+	fmt.Println()
+
+	arr = []int{400}
+	fmt.Println("Input:", arr)
+	ans = elements(arr)
+	fmt.Println("Answer 1:", ans)
+	fmt.Println()
+
+	fmt.Printf("Done running the problem in %s seconds.", time.Since(t0))
+	fmt.Println()
+}
+
+func elements(arr []int) []int {
+	bigRight := -1
+	for i := len(arr) - 1; i >= 0; i-- {
+		tmp := arr[i]
+		arr[i] = bigRight
+		if tmp > bigRight {
+			bigRight = tmp
+		}
+	}
+
+	return arr
+}
